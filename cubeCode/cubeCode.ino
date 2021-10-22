1ba838e5659bfa80b93754a4ee261da175b737e2
David McGinnis <dmcginnis427@gmail.com>
2021-10-22 08:12:08 +0200

    new cube code with modbus

--- cubeCode/cubeCode.ino
+++ cubeCode/cubeCode.ino
@@ -1,4 +1,6 @@
-#define BAUD_RATE 115200
+#include "ModbusRtu.h"
+#define BAUD_RATE  19200
+#define MODBUSBUFSIZE  11
 #define notEnabledPin 12
 #define m0Pin         11
 #define m1Pin         10
@@ -9,31 +11,43 @@
 #define dirPin         5
 #define notFaultPin    4
 
-#define blueLEDPin    13
+#define commLEDPin    13
 #define redLEDPin     21
 #define greenLEDPin   22
 #define yellowLEDPin  23
 
-#define stepsPerRevolution 200.0
+/**
+ *  Modbus object declaration
+ *  u8id : node id = 0 for master, = 1..247 for slave
+ *  port : serial port
+ *  u8txenpin : 0 for RS-232 and USB-FTDI 
+ *               or any pin number > 1 for RS-485
+ */
+Modbus slave(1,Serial1,0); // this is slave @1 and RS-232 or USB-FTDI
 
-struct TransmitData
+union ModbusUnion
 {
-  float nrevs = 0.0;
-};
-struct ReceiveData
-{
-  int moveMotor = 0;
-  int resetNrevReading = 0;
-  int noStepsToTurn = 0;
-  int stepDelayuS = 4000;
-  int stayAwakeAfterRotation = 0;
-  int resolution = 0;
-  int delayAfterRotationmS = 0;
-  int loopDelay = 1000;
-};
+  struct
+  {
+    uint16_t initCube;
+    uint16_t moveMotor;
+    uint16_t resetStepReading;
+    uint16_t noStepsToTurn;
+    uint16_t stepForward;
+    uint16_t stepDelayuS;
+    uint16_t stayAwakeAfterRotation;
+    uint16_t resolution;
+    uint16_t delayAfterRotationmS;
+    uint16_t stepLocation;
+    uint16_t stepLocationPositive;
+  };
+  uint16_t modbusBuffer[MODBUSBUFSIZE];
+} mb;
+int stepLocation = 0;
+boolean commLED = false;
+uint16_t msgCnt = 0;
 
-
-void setupPins()
+void setup()
 {
   pinMode(notEnabledPin, OUTPUT);
   pinMode(m0Pin,         OUTPUT);
@@ -44,7 +58,7 @@ void setupPins()
   pinMode(stepPin,       OUTPUT);
   pinMode(dirPin,        OUTPUT);
   pinMode(notFaultPin,   INPUT);
-  pinMode(blueLEDPin,    OUTPUT);
+  pinMode(commLEDPin,    OUTPUT);
   pinMode(redLEDPin,     OUTPUT);
   pinMode(greenLEDPin,   OUTPUT);
   pinMode(yellowLEDPin,  OUTPUT);
@@ -55,44 +69,130 @@ void setupPins()
   digitalWrite(m2Pin,         LOW);    
   digitalWrite(notResetPin,  HIGH);    
   digitalWrite(notSleepPin,   LOW);    
-  digitalWrite(blueLEDPin,    LOW);    
+  digitalWrite(commLEDPin,    LOW);    
   digitalWrite(redLEDPin,     LOW);    
   digitalWrite(greenLEDPin,   LOW);    
   digitalWrite(yellowLEDPin,  LOW);    
 
-  wakeup(0);   
+  mb.initCube               = 1;
+  mb.moveMotor              = 0;
+  mb.resetStepReading       = 0;
+  mb.noStepsToTurn          = 0;
+  mb.stepForward            = 1;
+  mb.stepDelayuS            = 10000;
+  mb.stayAwakeAfterRotation = 0;
+  mb.resolution             = 0;
+  mb.delayAfterRotationmS   = 100;
+  mb.stepLocation           = 0;
+  mb.stepLocationPositive   = 1;
+  
+  wakeup(0);
+  setResolution(mb.resolution);
+
+//  Serial.begin(9600);
+  Serial1.begin(BAUD_RATE);
+  slave.start();
+  delay(1000);
+//  for (int ii = 0; ii < 10; ++ii) Serial.println(mb.modbusBuffer[ii]);     
 }
-void wakeup(int wake)
+
+void loop()
+{
+  slave.poll( mb.modbusBuffer, MODBUSBUFSIZE );
+  checkComm();
+
+  setResolution(mb.resolution);
+  if (mb.resetStepReading > 0)
+  {
+    stepLocation = 0;
+    mb.stepLocation = 0;
+    mb.stepLocationPositive = 1;
+    mb.resetStepReading = 0;
+  }
+  wakeup(mb.stayAwakeAfterRotation);
+
+  if (mb.moveMotor > 0)
+  {
+    wakeup(1);
+    rotate(mb.noStepsToTurn, mb.stepDelayuS, mb.stepForward);
+    delay(mb.delayAfterRotationmS);
+    wakeup(mb.stayAwakeAfterRotation);
+    mb.moveMotor = 0;
+  }
+
+  delay(50);
+}
+void checkComm()
+{
+  uint16_t numMessages;
+  numMessages = slave.getInCnt();
+  if (numMessages != msgCnt)
+  {
+    msgCnt = numMessages;
+    commLED = !commLED;
+    digitalWrite(commLEDPin, commLED);    
+  }
+  
+}
+void incrementStepCounter()
+{
+    if (mb.stepForward > 0 )
+    {
+      stepLocation = stepLocation + 1;
+    }
+    else
+    {
+      stepLocation = stepLocation - 1;
+    }
+    if (stepLocation >= 0)
+    {
+      mb.stepLocation = (uint16_t) stepLocation;
+      mb.stepLocationPositive = 1;
+    }
+    else
+    {
+      mb.stepLocation = (uint16_t) -stepLocation;
+      mb.stepLocationPositive = 0;
+    }
+}
+void wakeup(uint16_t wake)
 {
   digitalWrite(redLEDPin,   wake);    
   digitalWrite(notSleepPin, wake);  
   delay(10);  
 }
-void rotate(int steps, int stepDelayuS)
+void rotate(uint16_t steps, uint16_t stepDelayuS, uint16_t stepForward)
 {
-  boolean direction = true;
-  if (steps < 0)
-  {
-    direction = false;
-    steps = -steps;
-  }
   digitalWrite(greenLEDPin,   HIGH);    
-  digitalWrite(yellowLEDPin,   direction);    
-  digitalWrite(dirPin, direction);
-  for (int i = 0; i < steps; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(stepDelayuS);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(stepDelayuS);
+  digitalWrite(yellowLEDPin,   stepForward);    
+  digitalWrite(dirPin, stepForward);
+  for (uint16_t i = 0; i < steps; i++) 
+  {
+    if (mb.moveMotor > 0)
+    {
+      // These four lines result in 1 step:
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds((int)stepDelayuS);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds((int) stepDelayuS);
+      incrementStepCounter();
+      slave.poll( mb.modbusBuffer, MODBUSBUFSIZE );
+      checkComm();
+    }
+    else
+    {
+      i = steps;
+      delay(mb.delayAfterRotationmS);
+      wakeup(mb.stayAwakeAfterRotation);
+    }
   }
   digitalWrite(greenLEDPin,   LOW);    
   digitalWrite(yellowLEDPin,   LOW);    
 }
-int setResolution(int ires)
+uint16_t setResolution(uint16_t ires)
 {
-  int iresSet = ires;
-  int iresSetDec = 1;
+  uint16_t iresSet = ires;
+  uint16_t iresSetDec = 1;
   if (ires < 0) iresSet = 0;
   if (ires > 5) iresSet = 5;
   switch (iresSet) 
@@ -139,106 +239,4 @@ int setResolution(int ires)
     iresSetDec = iresSetDec * 2;
   }  
   return iresSetDec;
-}
-
-void processNewSetting(TransmitData* tData, ReceiveData* rData, ReceiveData* newData)
-{
-  rData->noStepsToTurn = newData->noStepsToTurn;
-  rData->stepDelayuS = newData->stepDelayuS;
-  rData->stayAwakeAfterRotation = newData->stayAwakeAfterRotation;
-  rData->resolution = newData->resolution;
-  rData->delayAfterRotationmS = newData->delayAfterRotationmS;
-  rData->loopDelay = newData->loopDelay;
-  rData->moveMotor = newData->moveMotor;
-  rData->resetNrevReading = newData->resetNrevReading;
-
-  int iresSetDec = setResolution(rData->resolution);
-  if (rData->moveMotor > 0)
-  {
-    wakeup(1);
-    rotate(rData->noStepsToTurn, rData->stepDelayuS);
-    delay(rData->delayAfterRotationmS);
-    wakeup(rData->stayAwakeAfterRotation);
-    float deltaRevs = ((float) rData->noStepsToTurn) / ((float)iresSetDec) / stepsPerRevolution;
-    tData->nrevs = tData->nrevs + deltaRevs;
-  }
-  if (rData->resetNrevReading > 0)
-  {
-    tData->nrevs = 0.0;
-  }
-  wakeup(rData->stayAwakeAfterRotation);
-}
-boolean processData(TransmitData* tData, ReceiveData* rData)
-{
-  delay(rData->loopDelay);
-  return true;
-}
-
-const int microLEDPin = 13;
-const int commLEDPin = 13;
-boolean commLED = true;
-
-struct TXinfo
-{
-  int cubeInit = 1;
-  int newSettingDone = 0;
-};
-struct RXinfo
-{
-  int newSetting = 0;
-};
-
-struct TX
-{
-  TXinfo txInfo;
-  TransmitData txData;
-};
-struct RX
-{
-  RXinfo rxInfo;
-  ReceiveData rxData;
-};
-TX tx;
-RX rx;
-ReceiveData settingsStorage;
-
-int sizeOfTx = 0;
-int sizeOfRx = 0;
-
-void setup()
-{
-  setupPins();
-  pinMode(microLEDPin, OUTPUT);    
-  pinMode(commLEDPin, OUTPUT);  
-  digitalWrite(commLEDPin, commLED);
-//  digitalWrite(microLEDPin, commLED);
-
-  sizeOfTx = sizeof(tx);
-  sizeOfRx = sizeof(rx);
-  Serial1.begin(BAUD_RATE);
-  delay(1000);
-}
-void loop()
-{
-  boolean goodData = false;
-  goodData = processData(&(tx.txData), &settingsStorage);
-  if (goodData)
-  {
-    tx.txInfo.newSettingDone = 0;
-    if(Serial1.available() > 0)
-    { 
-      commLED = !commLED;
-      digitalWrite(commLEDPin, commLED);
-      Serial1.readBytes((uint8_t*)&rx, sizeOfRx);
-      
-      if (rx.rxInfo.newSetting > 0)
-      {
-        processNewSetting(&(tx.txData), &settingsStorage, &(rx.rxData));
-        tx.txInfo.newSettingDone = 1;
-        tx.txInfo.cubeInit = 0;
-      }
-    }
-    Serial1.write((uint8_t*)&tx, sizeOfTx);
-  }
-  
 }
